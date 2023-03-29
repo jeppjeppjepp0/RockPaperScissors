31d56bf82aef98a00a482464854ff4255f032c7b
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-03-29 13:07:52 -0600

    basic html, js, pseudocode

---
+++ script.js
@@ -0,0 +1,18 @@
+var wins = 0;
+var loss = 0;
+var ties = 0;
+
+function playRPS() {
+    var playerResponse = prompt("Enter Rock (R), Paper (P), or Scissors (S)");
+};
+
+/* 
+if press play, play
+prompt window: enter R, P, or S
+generates random response
+compare response and computer random
+alert WLT
+count win, loss, tie
+display all WLT
+confirm if user plays again
+*/
