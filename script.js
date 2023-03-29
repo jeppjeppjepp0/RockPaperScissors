415e4498e52f20ec5d959aa2b9da5dbdfde80a96
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-03-29 12:17:36 -0700

    put in the math floor formula to get random selection

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 var wins = 0;
 var loss = 0;
 var ties = 0;
+var RPS = ["R","P","S"]
 
 function playRPS() {
     var playerResponse = prompt("Enter Rock (R), Paper (P), or Scissors (S)");
+    var randomResponse = RPS[Math.floor(Math.random() * RPS.length)]
+
+    if playRPS()= randomeResponse {
+        alert("Tie!") {
+            
+        }
+
+    }
 };
 
 /* 
 if press play, play
 prompt window: enter R, P, or S
-generates random response
+-generates random response
 compare response and computer random
 alert WLT
 count win, loss, tie
